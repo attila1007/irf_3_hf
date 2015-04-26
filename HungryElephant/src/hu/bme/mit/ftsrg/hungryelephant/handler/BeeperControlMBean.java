6f9e204a085d5b30e5d4b205363a8eb5b92a4779
attila1007 <attila1007@gmail.com>
2015-04-26 15:40:24 +0200

    asd

---
+++ HungryElephant/src/hu/bme/mit/ftsrg/hungryelephant/handler/BeeperControlMBean.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.ftsrg.hungryelephant.handler;
+
+public interface BeeperControlMBean {
+	
+	public boolean getMuted();
+	public void setMuted(boolean muted);
+	
+	public int getCycleTimeInMsec();
+	public void setCycleTimeInMsec(int cycleTimeInMsec);
+	
+	public void exit();
+
+}
